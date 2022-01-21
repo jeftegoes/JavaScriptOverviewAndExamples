fe2f7c46b51de9cbc93d9081eb758b5c952d5e3d
jeftegoes <jefte.goes.dev@outlook.com>
2022-01-21 20:53:17 -0300

    First commit.

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System.Xml;
+
+var xmlString = @$" <post-method xmlns:S=""X"">
+                        <response-code>200</response-code>
+                        <response-desc>OK</response-desc>
+                        <entity>
+                            <content-type>text/xml</content-type>
+                            <content>
+                                <S:Body>
+                                    <S:execSyncResponse>
+                                        <S:return>
+                                            <S:errorType>U</S:errorType>
+                                            <S:serviceStatus>1</S:serviceStatus>
+                                            <S:serviceDescription/>
+                                            <S:serviceException>OK</S:serviceException>
+                                        </S:return>
+                                    </S:execSyncResponse>
+                                </S:Body>
+                            </content>
+                        </entity>
+                    </post-method>";
+
+var xmlDocument = new XmlDocument();
+xmlDocument.LoadXml(xmlString);
+
+var xmlNodeList = xmlDocument.GetElementsByTagName("S:return");
+
+foreach (XmlNode node in xmlNodeList)
+{
+    var result = @$"Error type: { node["S:errorType"].InnerText }
+Service status: { node["S:serviceStatus"].InnerText }
+Service description: { node["S:serviceDescription"].InnerText }
+Service exception: { node["S:serviceException"].InnerText }";
+
+    Console.WriteLine(result);
+}
---
+++ README.md
@@ -0,0 +1,7 @@
+## What is this project
+
+It is a small / simple project that shows how get specif elemnt into XML file.
+
+## Project Type Console
+
+.NET 6.0
